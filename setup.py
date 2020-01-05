f05d66e732f632e1352871b96a2ec74af26b2001
Michael Schwarz <michi.schwarz@gmail.com>
2020-01-05 20:58:19 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+    name='yet-another-duplicate-detector',
+    version='1.0.0',
+    packages=['yadd'],
+    entry_points=dict(
+        console_scripts=[]),
+    install_requires=[])
